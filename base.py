a5e43d7991850ac6b813c439552f5437a3e21c8f
Kha Ben <nhkhaben180909@gmail.com>
2025-02-26 21:43:51 -0700

    game base

---
+++ base.py
@@ -0,0 +1,9 @@
+choice = input("TEXT") 
+if choice == "1":
+    print("ENDING 1") 
+elif choice == "2":
+    print("ENDING 2") 
+elif choice == "3":
+    print("ENDING 3") 
+else:
+    print("Error PlaceHolder")  
